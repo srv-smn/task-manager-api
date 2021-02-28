d203fc1ae447400b7f8a0569db0453867071a62e
sourav <saurav2fly@gmail.com>
2021-02-28 19:13:03 +0530

    added extra features and authentication to task router and model /_\ end of folder 12

--- src/index.js
+++ src/index.js
@@ -5,18 +5,7 @@ const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
-
-// app.use((req, res, next)=>{
-//     if(req.method === 'GET'){
-//         res.send("Get Request are disabled")
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req,res,next) =>{
-//     res.status(503).send("Server under maintenance")
-// })
+ 
 
 app.use(express.json())
 app.use(userRouter)
