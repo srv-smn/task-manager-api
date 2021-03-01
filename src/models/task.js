842c9a430c9776310dadc6067dd2c0765bf2ec23
sourav <saurav2fly@gmail.com>
2021-03-01 14:09:48 +0530

    added filtering ,paging ,sorting feeatures -- end of folder 13

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-const Task = mongoose.model('Task',{
+
+const taskSchema = new mongoose.Schema({
     description:{
         type:String,
         trim:true,
@@ -15,6 +16,30 @@ const Task = mongoose.model('Task',{
         required:true,
         ref: 'User'
     }
+    
+},{
+    timestamps:true
 })
+const Task = mongoose.model('Task',taskSchema)
 
 module.exports = Task
+
+
+// const Task = mongoose.model('Task',{
+//     description:{
+//         type:String,
+//         trim:true,
+//         required:true
+//     },
+//     completed:{
+//         type:Boolean,
+//         default:false
+//     },
+//     owner:{
+//         type:mongoose.Schema.Types.ObjectId,
+//         required:true,
+//         ref: 'User'
+//     }
+// })
+
+// module.exports = Task
